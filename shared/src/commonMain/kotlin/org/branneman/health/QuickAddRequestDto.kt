afcc85b88e404ac0d58717f334708019809b3c54
Bran van der Meer <bran.van.der.meer@protonmail.com>
2026-06-11 20:47:00 +0200

    feat(shared): add QuickAddRequestDto

---
+++ shared/src/commonMain/kotlin/org/branneman/health/QuickAddRequestDto.kt
@@ -0,0 +1,11 @@
+package org.branneman.health
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class QuickAddRequestDto(
+    val id: String,
+    val quickAddKcal: Int,
+    val quickAddLabel: String? = null,
+    val loggedAt: String? = null,
+)
